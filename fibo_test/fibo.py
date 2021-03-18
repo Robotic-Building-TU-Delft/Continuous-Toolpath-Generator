2766ea86778ec802d4673251112655ea1d34ea2e
jurra <jose.urra86@gmail.com>
2021-03-18 14:43:10 +0100

    Fixed syntax error and reverted changes

--- fibo_test/fibo.py
+++ fibo_test/fibo.py
@@ -1,10 +1,3 @@
-def fib(n):    # write Fibonacci series up to n
-    a, b = 0, 1
-    while a < n:
-        print(a, end=' ')
-        a, b = b, a+b
-    print()
-
 def fib2(n):   # return Fibonacci series up to n
     result = []
     a, b = 0, 1
