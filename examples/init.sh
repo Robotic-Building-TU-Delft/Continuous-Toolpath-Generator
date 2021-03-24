c262f84678c9b61944c92e3a03fc11c5796186b2
jurra <jose.urra86@gmail.com>
2021-03-24 08:02:50 +0100

    Wrok with rhino working directory, started setting up a simple bash init workflow that can allow to detach python scripts from inside Rhino

--- examples/init.sh
+++ examples/init.sh
@@ -3,7 +3,4 @@ file_name="fibo.py"
 env=$PWD"/"$file_name
 touch $env
 echo $env > .env
-# touch file_path
-# env > $PWD"/"$file_name
-# nano  $env -f end-of-buffer
-# start fibo_test.gh
+start fibo_test.gh
