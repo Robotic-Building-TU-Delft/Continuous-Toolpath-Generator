373e7f630c21a29d172470630b0792583feb1cb5
jurra <jose.urra86@gmail.com>
2021-03-24 07:55:28 +0100

    Refactoring example and setting up .env variables with bash to dinamically update path

---
+++ examples/init.sh
@@ -0,0 +1,9 @@
+# !/bin/sh
+file_name="fibo.py"
+env=$PWD"/"$file_name
+touch $env
+echo $env > .env
+# touch file_path
+# env > $PWD"/"$file_name
+# nano  $env -f end-of-buffer
+# start fibo_test.gh
